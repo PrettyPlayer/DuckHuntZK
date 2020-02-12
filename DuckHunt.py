4c72a0ea62fc5046294186ccb787b2918d1c245c
zlidenskardanom <60474799+zlidenskardanom@users.noreply.github.com>
2020-02-12 13:10:14 +0300

    Add files via upload
    оптимизация кода, вынесение параметров для быстрого редактирования

--- DuckHunt.py
+++ DuckHunt.py
@@ -7,7 +7,6 @@ FPS = 60
 WIDTH = 1920
 HEIGHT = 1080
 fullscreen = 1
-format = ".png"
 
 #Цвета
 BLACK = (0, 0, 0)
@@ -18,45 +17,58 @@ RED = (255, 0, 0)
 CYAN = (0, 255, 255)
 GREEN = (0, 255, 0)
 
-#Параметры мыши
+		#Параметры мыши
+#Расстояние начала палочек мыши
 mouse_start_len = 3
+#Длина палочек мыши
 mouse_len = 7
+#Толщина палочек мыши
 mouse_thickness = 3
+#Цвет мыши в меню
+mouse_color_menu = RED
+#Цвет мыши в игре
+mouse_color_game = GREEN
+
+		#Параметры
+#Изменение скорости уток (1..Inf)
+duck_acc=1
 
 #Вёрстка окна
 pygame.init()
 surf_main = pygame.display.set_mode((WIDTH, HEIGHT), pygame.FULLSCREEN)
 pygame.display.set_caption("DuckHuntZK")
-img = pygame.image.load(os.path.realpath("") + r"\Sprites\Icon.png")
+img = pygame.image.load(os.path.realpath("Sprites\\Icon.png"))
 pygame.display.set_icon(img)
 clock = pygame.time.Clock()
 pygame.mouse.set_visible(False)
 
 #Прицел вместо мыши
-def mouse_replace(color, mouse_start_len, mouse_len, mouse_thickness):
+def mouse_replace_menu(mouse_color_menu, mouse_start_len, mouse_len, mouse_thickness):
 	if pygame.mouse.get_focused():
 		pos = pygame.mouse.get_pos()
-		pygame.draw.line(surf_main, color, (pos[0], pos[1] + mouse_start_len), (pos[0], pos[1] + mouse_start_len + mouse_len), mouse_thickness)
-		pygame.draw.line(surf_main, color, (pos[0], pos[1] - mouse_start_len), (pos[0], pos[1] - mouse_start_len - mouse_len), mouse_thickness)
-		pygame.draw.line(surf_main, color, (pos[0] + mouse_start_len, pos[1]), (pos[0] + mouse_start_len + mouse_len, pos[1]), mouse_thickness)
-		pygame.draw.line(surf_main, color, (pos[0] - mouse_start_len, pos[1]), (pos[0] - mouse_start_len - mouse_len, pos[1]), mouse_thickness)
+		pygame.draw.line(surf_main, mouse_color_menu, (pos[0], pos[1] + mouse_start_len), (pos[0], pos[1] + mouse_start_len + mouse_len), mouse_thickness)
+		pygame.draw.line(surf_main, mouse_color_menu, (pos[0], pos[1] - mouse_start_len), (pos[0], pos[1] - mouse_start_len - mouse_len), mouse_thickness)
+		pygame.draw.line(surf_main, mouse_color_menu, (pos[0] + mouse_start_len, pos[1]), (pos[0] + mouse_start_len + mouse_len, pos[1]), mouse_thickness)
+		pygame.draw.line(surf_main, mouse_color_menu, (pos[0] - mouse_start_len, pos[1]), (pos[0] - mouse_start_len - mouse_len, pos[1]), mouse_thickness)
+
+def mouse_replace_game(mouse_mouse_color_game_game, mouse_start_len, mouse_len, mouse_thickness):
+	if pygame.mouse.get_focused():
+		pos = pygame.mouse.get_pos()
+		pygame.draw.line(surf_main, mouse_color_game, (pos[0], pos[1] + mouse_start_len), (pos[0], pos[1] + mouse_start_len + mouse_len), mouse_thickness)
+		pygame.draw.line(surf_main, mouse_color_game, (pos[0], pos[1] - mouse_start_len), (pos[0], pos[1] - mouse_start_len - mouse_len), mouse_thickness)
+		pygame.draw.line(surf_main, mouse_color_game, (pos[0] + mouse_start_len, pos[1]), (pos[0] + mouse_start_len + mouse_len, pos[1]), mouse_thickness)
+		pygame.draw.line(surf_main, mouse_color_game, (pos[0] - mouse_start_len, pos[1]), (pos[0] - mouse_start_len - mouse_len, pos[1]), mouse_thickness)
 
 def fullscreen(fullscreen):
 	if fullscreen == 0:
-		pygame.display.set_mode((1920, 1080), pygame.FULLSCREEN)
+		pygame.display.set_mode((WIDTH, HEIGHT), pygame.FULLSCREEN)
 		fullscreen=1
 	else:
-		pygame.display.set_mode((1920, 1080))
+		pygame.display.set_mode((WIDTH, HEIGHT))
 		fullscreen=0
 
-def path(folder, filename):
-	return (os.path.realpath("") + folder + filename + format)
-
-def path2(folder, folder2, filename):
-	return (os.path.realpath("") + folder + folder2 + filename + ".png")
-
 def shot(pos):
-	shotimg = pygame.image.load(path("\Sprites\shot\\", str(randint(1,3))))
+	shotimg = pygame.image.load(os.path.realpath("Sprites\\shot\\" + str(randint(1,3) + ".png")))
 	shotimg = pygame.transform.scale(shotimg, (int(shotimg.get_width()*1), int(shotimg.get_height()*1)))
 	shotrect = shotimg.get_rect(center=(pos[0], pos[1]))
 
@@ -65,7 +77,7 @@ class Button(pygame.sprite.Sprite):
 		pygame.sprite.Sprite.__init__(self)
 		self.x=x
 		self.y=y
-		self.image = pygame.image.load(path(r"\Sprites\button\\", filename))
+		self.image = pygame.image.load(os.path.realpath("Sprites\\button\\" + filename + ".png"))
 		self.image = pygame.transform.scale(self.image, (int(self.image.get_width()*size), int(self.image.get_height()*size)))
 		self.rect = self.image.get_rect(center=(x, y))
 		self.sx=self.image.get_width()
@@ -82,7 +94,7 @@ class Button(pygame.sprite.Sprite):
 class Duck(pygame.sprite.Sprite):
 	def __init__(self, duck_x, duck_y, size, acc, filename, name):
 		pygame.sprite.Sprite.__init__(self)
-		self.image = pygame.image.load(path(r"\Sprites\black\\", filename))
+		self.image = pygame.image.load(os.path.realpath("Sprites\\black\\" + filename + ".png"))
 		self.image.set_colorkey((187, 238, 167))
 		self.image = pygame.transform.scale(self.image, (int(self.image.get_width()*size), int(self.image.get_height()*size)))
 		self.rect = self.image.get_rect(center=(duck_x, duck_y))
@@ -101,8 +113,6 @@ class Duck(pygame.sprite.Sprite):
 		self.counter=0
 		self.name=name
 		self.size=size
-	def spriteduck(self, nameduck, name):
-		self.image = pygame.image.load(path(r"\Sprites\black\\", name))
 	def fly(self):
 		if self.hp>0:
 			if self.time==0:
@@ -134,34 +144,24 @@ class Duck(pygame.sprite.Sprite):
 			#Смена спрайта каждые counter тиков и в зависимости от направления движения
 			if abs(self.dir[0])<abs(self.dir[1]) and self.dir[1]<0:
 				if 0<=self.counter%15<=4:
-					self.image = pygame.image.load(path2(r"\Sprites\\", r"black\\", "duck1"))
-					if self.dirname0==0:
-						self.image = pygame.transform.flip(self.image, 1, 0)
+					self.image = pygame.image.load(os.path.realpath("Sprites\\" + self.name + "\\duck1.png"))
 				elif 5<=self.counter%15<=9:
-					self.image = pygame.image.load(path2(r"\Sprites\\", r"black\\", "duck2"))
-					if self.dirname0==0:
-						self.image = pygame.transform.flip(self.image, 1, 0)
+					self.image = pygame.image.load(os.path.realpath("Sprites\\" + self.name + "\\duck2.png"))
 				elif 10<=self.counter%15<=14:
-					self.image = pygame.image.load(path2(r"\Sprites\\", r"black\\", "duck3"))
-					if self.dirname0==0:
-						self.image = pygame.transform.flip(self.image, 1, 0)
+					self.image = pygame.image.load(os.path.realpath("Sprites\\" + self.name + "\\duck3.png"))
 				self.image = pygame.transform.scale(self.image, (int(self.image.get_width()*self.size), int(self.image.get_height()*self.size)))
 			elif abs(self.dir[0])>abs(self.dir[1]) or abs(self.dir[0])<abs(self.dir[1]) and self.dir[1]>0:
 				if 0<=self.counter%15<=4:
-					self.image = pygame.image.load(path2(r"\Sprites\\", r"black\\", "duck4"))
-					if self.dirname0==0:
-						self.image = pygame.transform.flip(self.image, 1, 0)
+					self.image = pygame.image.load(os.path.realpath("Sprites\\" + self.name + "\\duck4.png"))
 				elif 5<=self.counter%15<=9:
-					self.image = pygame.image.load(path2(r"\Sprites\\", r"black\\", "duck5"))
-					if self.dirname0==0:
-						self.image = pygame.transform.flip(self.image, 1, 0)
+					self.image = pygame.image.load(os.path.realpath("Sprites\\" + self.name + "\\duck5.png"))
 				elif 10<=self.counter%15<=14:
-					self.image = pygame.image.load(path2(r"\Sprites\\", r"black\\", "duck6"))
-					if self.dirname0==0:
-						self.image = pygame.transform.flip(self.image, 1, 0)
+					self.image = pygame.image.load(os.path.realpath("Sprites\\" + self.name + "\\duck6.png"))
 				self.image = pygame.transform.scale(self.image, (int(self.image.get_width()*self.size), int(self.image.get_height()*self.size)))
-			self.x+=self.dir[0]*self.acc
-			self.y+=self.dir[1]*self.acc
+			if self.dirname0==0:
+				self.image = pygame.transform.flip(self.image, 1, 0)
+			self.x+=self.dir[0]*self.acc*duck_acc
+			self.y+=self.dir[1]*self.acc*duck_acc
 			self.image.set_colorkey((187, 238, 167))
 			self.rect = self.image.get_rect(center=(int(self.x), int(self.y)))
 			self.time-=1
@@ -177,7 +177,7 @@ class Duck(pygame.sprite.Sprite):
 class Trace(pygame.sprite.Sprite):
 	def __init__(self, pos, name, num):
 		pygame.sprite.Sprite.__init__(self)
-		self.image = pygame.image.load(path("\Sprites\shot\\", name))
+		self.image = pygame.image.load(os.path.realpath("Sprites\\shot\\" + name + ".png"))
 		self.image = pygame.transform.scale(self.image, (int(self.image.get_width()*0.2), int(self.image.get_height()*0.2)))
 		self.rect = self.image.get_rect(center=(pos[0], pos[1]))
 		self.num=num
@@ -196,7 +196,7 @@ class Trace(pygame.sprite.Sprite):
 class Weapon(pygame.sprite.Sprite):
 	def __init__(self, name):
 		pygame.sprite.Sprite.__init__(self)
-		self.image = pygame.image.load(path2("\Sprites\weapon\\", name, "1"))
+		self.image = pygame.image.load(os.path.realpath("Sprites\\weapon\\" + name + "\\1.png"))
 		self.image = pygame.transform.scale(self.image, (int(self.image.get_width()*5), int(self.image.get_height()*5)))
 		self.rect = self.image.get_rect(center=(WIDTH//2+300, 900))
 		self.alpha=-30
@@ -216,10 +216,10 @@ class Weapon(pygame.sprite.Sprite):
 ##Начало игры#Начало игры#Начало игры#Начало игры#Начало игры#Начало игры#Начало игры#Начало игры#Начало игры#Начало игры#Начало игры#Начало игры##
 ###################################################################################################################################################
 def GameWindow():
-	frontwindow = pygame.image.load(os.path.realpath("") + r"\Sprites\front.png").convert_alpha()
+	frontwindow = pygame.image.load(os.path.realpath("Sprites\\front.png")).convert_alpha()
 	frontwindow_rect = frontwindow.get_rect(topleft=(0, 0))
 	
-	backwindow = pygame.image.load(os.path.realpath("") + r"\Sprites\back.png").convert_alpha()
+	backwindow = pygame.image.load(os.path.realpath("Sprites\\back.png")).convert_alpha()
 	backwindow_rect = backwindow.get_rect(topleft=(0, 0))
 	
 	trace1 = Trace((-200, -200), "trace1", 1)
@@ -288,7 +288,7 @@ def GameWindow():
 		surf_main.blit(frontwindow, frontwindow_rect)
 		
 		#Замена мыши
-		mouse_replace(GREEN, mouse_start_len, mouse_len, mouse_thickness)
+		mouse_replace_game(mouse_color_game, mouse_start_len, mouse_len, mouse_thickness)
 		
 		surf_main.blit(pistol.image2, pistol.rect)
 		#Обновление дисплея
@@ -301,44 +301,47 @@ def GameWindow():
 ##################################################################################################################################################
 ##Главное меню#Главное меню#Главное меню#Главное меню#Главное меню#Главное меню#Главное меню#Главное меню#Главное меню#Главное меню#Главное меню##
 ##################################################################################################################################################
-button1 = Button(400, 200, 8, "button1", "Играть", "times.ttf", 36, RED)
-button2 = Button(400, 400, 8, "button1", "Магазин", "times.ttf", 36, RED)
-button3 = Button(400, 600, 8, "button1", "Настройки", "times.ttf", 36, RED)
-
-menuwindow = pygame.image.load(os.path.realpath("") + r"\Sprites\menuwindow.png")
-menuwindow_rect = menuwindow.get_rect(topleft=(0, 0))
-
-while True:
-	surf_main.blit(menuwindow, menuwindow_rect)
-	for event in pygame.event.get():
-		if event.type == pygame.QUIT:
-			quit()
-		elif event.type == pygame.MOUSEBUTTONDOWN:
-			button1.click(event.pos)
-			button2.click(event.pos)
-			button3.click(event.pos)
-		elif event.type == pygame.KEYDOWN:
-			if event.key == pygame.K_ESCAPE:
+def MenuWindow():
+	button1 = Button(400, 200, 8, "button1", "Играть", "times.ttf", 36, RED)
+	button2 = Button(400, 400, 8, "button1", "Магазин", "times.ttf", 36, RED)
+	button3 = Button(400, 600, 8, "button1", "Настройки", "times.ttf", 36, RED)
+	
+	menuwindow = pygame.image.load(os.path.realpath("Sprites\\menuwindow.png"))
+	menuwindow_rect = menuwindow.get_rect(topleft=(0, 0))
+	
+	while True:
+		surf_main.blit(menuwindow, menuwindow_rect)
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
 				quit()
-				sys.exit()
-			if event.key == pygame.K_RETURN:
-				fullscreen(fullscreen)
-	
-	#Отображение кнопок на фоне меню
-	menuwindow.blit(button1.image, button1.rect)
-	menuwindow.blit(button2.image, button2.rect)
-	menuwindow.blit(button3.image, button3.rect)
+			elif event.type == pygame.MOUSEBUTTONDOWN:
+				if event.button == 1:
+					button1.click(event.pos)
+					button2.click(event.pos)
+					button3.click(event.pos)
+			elif event.type == pygame.KEYDOWN:
+				if event.key == pygame.K_ESCAPE:
+					quit()
+					sys.exit()
+				if event.key == pygame.K_RETURN:
+					fullscreen(fullscreen)
+		
+		#Отображение кнопок на фоне меню
+		menuwindow.blit(button1.image, button1.rect)
+		menuwindow.blit(button2.image, button2.rect)
+		menuwindow.blit(button3.image, button3.rect)
+		#Отображение текста на кнопках
+		button1.image.blit(button1.text, button1.place)
+		button2.image.blit(button2.text, button2.place)
+		button3.image.blit(button3.text, button3.place)
+		
+		#Замена мыши
+		mouse_replace_menu(mouse_color_menu, mouse_start_len, mouse_len, mouse_thickness)
+		
+		#Обновление дисплея
+		pygame.display.update()
+		
+		#Задержка (FPS)
+		clock.tick(FPS*1.2)
 
-	#Отображение текста на кнопках
-	button1.image.blit(button1.text, button1.place)
-	button2.image.blit(button2.text, button2.place)
-	button3.image.blit(button3.text, button3.place)
-	
-	#Замена мыши
-	mouse_replace(RED, mouse_start_len, mouse_len, mouse_thickness)
-	
-	#Обновление дисплея
-	pygame.display.update()
-	
-	#Задержка (FPS)
-	clock.tick(FPS)
+MenuWindow()
